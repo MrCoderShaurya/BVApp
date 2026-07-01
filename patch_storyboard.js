4a15587d4050abf9eb6b9b9782accaddfbcbbce0
Shaurya Omm <shauryaomm@gmail.com>
2026-07-01 17:43:56 +0530

    fix: remove Cordova robot logo and imageView constraints from launch storyboard

--- patch_storyboard.js
+++ patch_storyboard.js
@@ -37,11 +37,17 @@ if (storyboardPath && fs.existsSync(storyboardPath)) {
   const bgRegex = /<color key="backgroundColor" [^>]*\/>/g;
   if (content.match(bgRegex)) {
     content = content.replace(bgRegex, '<color key="backgroundColor" white="1" alpha="1" colorSpace="calibratedWhite"/>');
-    fs.writeFileSync(storyboardPath, content, 'utf8');
-    console.log('Successfully patched backgroundColor to classic calibratedWhite!');
-  } else {
-    console.log('Could not find backgroundColor tag to patch.');
   }
+
+  // Strip out the Cordova robot imageView tag
+  content = content.replace(/<imageView[^>]*image="LaunchStoryboard"[^>]*>[\s\S]*?<\/imageView>/g, '');
+
+  // Strip out constraints that reference the removed imageView ID (2ns-9I-Qjs)
+  content = content.replace(/<constraint[^>]*2ns-9I-Qjs[^>]*\/>/g, '');
+
+  // Write changes back to storyboard file
+  fs.writeFileSync(storyboardPath, content, 'utf8');
+  console.log('Successfully patched storyboard to remove the Cordova robot logo and background constraints!');
 } else {
   console.error('ERROR: CDVLaunchScreen.storyboard not found under platforms/ios');
   process.exit(1);
